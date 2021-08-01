08709687e5bf7e1ee1e9a856fd4c7358c3aad35a
Tristan Gauci <tristan@maega.com.au>
2021-08-01 23:55:29 +1000

    Implement basic boot entrypoint

--- boot.js
+++ boot.js
@@ -2,13 +2,41 @@ const {app, BrowserWindow} = require('electron');
 
 function createWindow() {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600
+        width: 1920,
+        height: 1200
     })
 
     win.loadFile('index.html')
 }
 
+function createAppWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        transparent: true,
+        frame: false,
+        resizable: false
+    })
+
+    win.loadFile('index.html')
+}
+
+function launchApp(appId) {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        //transparent: true,
+        //frame: false,
+        //resizable: false
+    })
+
+    win.loadFile(`apps/${appId}/main.html`);
+}
+
 app.whenReady().then(() => {
-    createWindow()
+    createWindow();
+    //createAppWindow();
+
+    launchApp('settings');
+
 });
