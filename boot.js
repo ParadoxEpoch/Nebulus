ae3981ca1b906b14b763183b5cec26e7a5e96ce5
Tristan Gauci <tristan@maega.com.au>
2021-08-10 16:13:25 +1000

    Make main window fullscreen and frameless

--- boot.js
+++ boot.js
@@ -3,34 +3,28 @@ const {app, BrowserWindow} = require('electron');
 function createWindow() {
     const win = new BrowserWindow({
         width: 1920,
-        height: 1200
-    })
-
-    win.loadFile('index.html')
-}
-
-function createAppWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        transparent: true,
+        height: 1200,
         frame: false,
-        resizable: false
+        fullscreen: true
     })
 
-    win.loadFile('index.html')
+    win.loadFile('index.html');
+
+    //win.openDevTools();
 }
 
 function launchApp(appId) {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
-        //transparent: true,
+        transparent: true,
         //frame: false,
         //resizable: false
     })
 
     win.loadFile(`apps/${appId}/main.html`);
+
+    //win.openDevTools();
 }
 
 app.whenReady().then(() => {
